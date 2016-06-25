30ced5d553292e75b16eafdeda7bbd6c3bf56133
swebalasubu <swetha20.kv@gmail.com>
2016-06-25 11:26:05 +0530

    Create sumofminandmax.c

---
+++ sumofminandmax.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+
+
+int main(void)
+
+{
+	
+
+ 
+int n;
+	
+
+ int a[n],i,j, min=0, max=0;
+	
+ 
+
+ printf("enter the number of elements \n");
+	
+
+
+ scanf("%d",&n);
+	  
+
+ 
+printf("enter the elements of the array\n");
+	 
+ 
+
+ for(i=0;i<n;i++)
+
+ {
+  
+
+ scanf("%d",&a[i]);
+	 
+  
+  if(i == 1)
+  {
+     
+   if (a[0]>a[1])
+   {
+      
+    max = a[0];
+      
+    min = a[1];
+    
+   }
+   else
+   {
+      
+    max = a[1];
+      
+    min = a[0];
+    
+   }
+  
+  }
+  else if(i>1)
+  {
+   
+    
+   if(a[i]>max)
+   {
+        
+      
+    max = a[i];
+       
+    
+   } 
+    
+   if(a[i]<min)
+   {
+     
+      
+    min = a[i];
+        
+    
+   } 
+ 
+  
+  }
+ 
+
+ }
+
+printf(" %d  %d",a[0],a[1]); 
+
+printf("sum of the Maximum  and Minimum elements is %d",max+min);
+ 
+return 0;
+
+
+}
