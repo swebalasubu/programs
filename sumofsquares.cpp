ff181a87c8401010560bd90fdf7ff28f988c3cf4
swebalasubu <swetha20.kv@gmail.com>
2016-06-25 10:42:19 +0530

    Create sumofsquares.cpp

---
+++ sumofsquares.cpp
@@ -0,0 +1,24 @@
+#include<iostream.h>
+#include<string.h>
+#include<math.h>
+
+void main()
+{
+ 
+    int i=0,n;
+    cout<<"Please give the number of elements for the array\n"
+    cin>>n;
+    int a[n],individualNumber,sum=0;
+    for(i=0;i<n;i++)
+    {
+      cout<<"Please give the %d th element<<i\n"
+      cin>>a[i];
+      individualNumber = a[i];
+      if((i+1)%2==0)
+       sum=(individualNumber*individualNumber)+sum;
+      else
+       sum=(individualNumber*individualNumber*individualNumber)+sum;
+     }
+     
+    cout<<sum;
+}  
